4af0278cf79eb9a2d5624c8af82a57680b770c5c
Marcelo Primo <marceloprimo@bitbucket.org>
2024-04-23 12:20:06 -0300

    chore: Some fixes and updates

--- roles/workstation/files/sops_install.sh
+++ roles/workstation/files/sops_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 VERSION_SOPS=$(curl -s https://api.github.com/repos/getsops/sops/releases/latest | grep tag_name | cut -d '"' -f 4)
-curl -LO https://github.com/getsops/sops/releases/download/"$VERSION_SOPS"/sops-"$VERSION_SOPS".linux
-mv sops-"$VERSION_SOPS".linux /usr/local/bin/sops
+curl -LO https://github.com/getsops/sops/releases/download/"$VERSION_SOPS"/sops-"$VERSION_SOPS".linux.amd64
+mv sops-"$VERSION_SOPS".linux.amd64 /usr/local/bin/sops
 chmod +x /usr/local/bin/sops
